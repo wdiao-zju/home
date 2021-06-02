75497ab45d4ba256e2a7836f7c1b446533299db3
wdiao <424259600@qq.com>
2021-06-02 22:56:29 +0800

    1

--- build_blog.R
+++ build_blog.R
@@ -1,13 +1,14 @@
 ## 域名地址
 "https://github.com/ShixiangWang/home"
-"https://wdiao.netlify.app/"
+"https://wdiao-zju.netlify.app/"
 
 ## build
 # blogdown::new_site(theme = "yihui/hugo-xmin")
 blogdown::serve_site()
 
-post_name <- "ggplot-barplot"
-blogdown::new_post(post_name, ext = ".Rmd", slug = "barplot")
+title_name <- "ggplot-title"
+slug_name <- "ggplot-slug"
+blogdown::new_post(title = title_name, slug = slug_name, ext = ".Rmd")
 blogdown::hugo_build()
 
 # blogdown::yaml2toml("config.yaml", "config.toml")
