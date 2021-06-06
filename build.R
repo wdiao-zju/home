74dd8deee671dc42c23022ab1639767c2ef4cedf
wdiao <424259600@qq.com>
2021-06-06 15:32:01 +0800

    modify css

--- build.R
+++ build.R
@@ -4,33 +4,28 @@
 "https://app.netlify.com/sites/wdiao-zju/overview"
 
 ## build
-```{r}
 # blogdown::new_site(theme = "yihui/hugo-xmin")
 blogdown::serve_site()
 # blogdown::check_site()
-# blogdown::hugo_build()
+blogdown::hugo_build()
 # blogdown::build_site(build_rmd = T)
-```
+
 
 ## Rmd
-```{r}
-name <- "R-regular-expression"
+name <- "forcats"
 blogdown::new_post(title = name, slug = name, ext = ".Rmd")
-```
 
 ## md
-```{r}
 name <- "ubuntu-desktop"
 blogdown::new_post(title = name, slug = name, ext = ".md")
-```
+
 
 ## other
-```{r}
 # blogdown::yaml2toml("config.yaml", "config.toml")
 # blogdown::toml2yaml("config.toml", "config.yaml")
 
 # blogdown::config_netlify()
 
 # install.packages("ggstatsplot")
-```
+
 
